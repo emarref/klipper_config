9a27302211a237c35e3eb396e2d07097d2490310
Malcolm Fell <emarref@users.noreply.github.com>
2025-05-23 10:34:29 +1200

    fix: update autocommit to use commitron for generating commit messages
    Replace manual commit message creation with commitron for generating commit messages in the autocommit script.
    This change improves the commit message generation process by using commitron instead of manually specifying each message field.

--- autocommit.sh
+++ autocommit.sh
@@ -59,7 +59,8 @@ push_config() {
   git pull
   git add .
   current_date=$(date +"%Y-%m-%d %T")
-  git commit -m "Autocommit from ${current_date}" -m "${m1}" -m "${m2}" -m "${m3}" -m "${m4}"
+  commitron generate --config ./.commitronrc
+  #git commit -m "Autocommit from ${current_date}" -m "${m1}" -m "${m2}" -m "${m3}" -m "${m4}"
   git push
 }
 
