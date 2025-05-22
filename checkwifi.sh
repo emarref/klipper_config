96406e850eac445dd4a3267caf033b252d32c557
Malcolm Fell <emarref@users.noreply.github.com>
2025-05-23 09:29:41 +1200

    Autocommit from 2025-05-23 09:29:41
    Klipper on commit: 94a5d37
    
    Moonraker on commit: 1c12414
    
    Fluidd version: v1.34.2

--- checkwifi.sh
+++ checkwifi.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# https://gist.github.com/draptik/9f4b472da9f315aeeb3b
 # http://weworkweplay.com/play/rebooting-the-raspberry-pi-when-it-loses-wireless-connection-wifi/
 
 # Check wifi connection...
@@ -8,11 +9,11 @@ ping -c4 192.168.117.1 > /dev/null
 # If exit code != 0 (failure)...
 if [ $? != 0 ]
 then
-  echo "$(date): No network connection, restarting wlan0" >> /home/emarref/printer_data/logs/bouncewifi.log
+  echo "$(date): No network connection, restarting wlan0" >> /home/emarref/printer_data/logs/checkwifi.log
 
   # deactivate wifi...
-  /sbin/ifdown 'wlan0'
+  ip link set wlan0 down
   sleep 5
   # Restart wifi...
-  /sbin/ifup --force 'wlan0'
+  ip link set wlan0 up
 fi
