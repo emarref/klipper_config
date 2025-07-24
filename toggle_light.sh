d43846a31303f8d54cfe342a81fcd253c7b8592d
Malcolm Fell <emarref@users.noreply.github.com>
2025-07-24 18:07:17 +1200

    Source env from .env file

--- toggle_light.sh
+++ toggle_light.sh
@@ -2,4 +2,7 @@
 
 STATE="${1:-TOGGLE}"
 
-mosquitto_pub -u printer -P $MQTT_PASSWORD -h 'homeassistant.local.' -t 'zigbee2mqtt/Office Light Switch/set' -m "{\"state_l1\":\"$STATE\"}"
+cd "$(dirname "$0")"
+set -a && source .env && set +a
+
+mosquitto_pub -u printer -P "$MQTT_PASSWORD" -h "homeassistant.local" -t "zigbee2mqtt/Office Light Switch/set" -m "{\"state_l1\":\"$STATE\"}"
